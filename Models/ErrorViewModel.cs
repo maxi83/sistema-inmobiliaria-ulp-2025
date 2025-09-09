e209004721d92fa941bddb1408c180e1fc52b968
Maximiliano <maximilianogatica83@gmail.com>
2025-09-09 19:18:52 -0300

    Primer Modelo

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,4 +1,8 @@
-namespace sistema_inmobiliaria_ulp_2025.Models;
+/*namespace es nombre asignado para un grupo de archivos.
+es un nombre de espcio que funciona como contenedor absracto que agrupa 
+identificadores únicos como clases, funciones y variables permitiendo que
+ el mismo nombre se utilicen en diferentes contextos sin conflictos*/
+ namespace sistema_inmobiliaria_ulp_2025.Models;
 
 public class ErrorViewModel
 {
---
+++ Models/Inquilino.cs
@@ -0,0 +1,6 @@
+ namespace sistema_inmobiliaria_ulp_2025.Models;
+public class Inquilino{ 
+    
+ }
+
+ 
