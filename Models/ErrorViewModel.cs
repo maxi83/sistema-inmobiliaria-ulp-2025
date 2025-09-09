b65d3653c3326d02b2537276fd7aff919982cff2
Maximiliano <maximilianogatica83@gmail.com>
2025-09-09 18:49:56 -0300

    Primer Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace sistema_inmobiliaria_ulp_2025.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
